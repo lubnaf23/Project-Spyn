f8f736498aa49d5b78ab0bfefa8de34a0b521ce0
lubnaf23 <157326176+lubnaf23@users.noreply.github.com>
2024-02-28 11:24:26 -0700

    Update Test.m

--- Test.m
+++ Test.m
@@ -1,7 +1,9 @@
 brick.MoveMotor('B', -40); %forward
-distance = brick.UltrasonicDist(2); %sensor
-if distance <= 20 
-    brick.StopMotor('B', 'Coast'); %unlocked state
+%sensor
+while true
+   distance = brick.UltrasonicDist(2); 
+   if distance <= 20
+       brick.StopMotor('B', 'Coast'); %unlocked state
+   end
 
 end
-display(distance);   
