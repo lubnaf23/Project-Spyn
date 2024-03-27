f6c6b80a37e0a7a3e134b40c270fb292a7c8f527
lubnaf23 <157326176+lubnaf23@users.noreply.github.com>
2024-03-27 11:41:45 -0700

    Update milestone2.m

--- milestone2.m
+++ milestone2.m
@@ -1,23 +1,38 @@
+brick.SetColorMode(3, 2);
+
 while 1
 brick.MoveMotor('B',30); %forward
-brick.MoveMotor('D',30);
-color = brick.SetColorMode(3, 2);
+brick.MoveMotor('C',30);
+color = brick.ColorCode(3);
+
 
 if (color == 5) 
     brick.StopMotor('B');
-    brick.StopMotor('D');
+    brick.StopMotor('C');
     pause(1.0);
 end
 
 if (color == 2)
+    brick.StopMotor('B');
+    brick.StopMotor('C');
     brick.playTone(100,300,500);
+    pause(1);
     brick.playTone(100,300,500);
+    pause(3);
 end
 
 if (color == 3)
+    brick.StopMotor('B');
+    brick.StopMotor('C');
+   
+    %brick.beep(3);
+    
     brick.playTone(100,300,500);
+    pause(1);
     brick.playTone(100,300,500);
+    pause(1);
     brick.playTone(100,300,500);
+    pause(3);
+   
 end
 end
-
