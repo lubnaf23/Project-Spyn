27bcb9d2fd04643ad8bcb85d75b17552835a0043
liamreynolds222 <165196517+liamreynolds222@users.noreply.github.com>
2024-03-27 10:55:22 -0700

    Update RemoteControl.m

--- RemoteControl.m
+++ RemoteControl.m
@@ -1,6 +1,37 @@
-brick.MoveMotor('A', 50); %didn't test
-brick.MoveMotor('B', -40); %forward
-brick.MoveMotor('C', -17); %up
-brick.MoveMotor('C', 10); %down
+global key
+InitKeyboard();
+
+while 1
+    pause(0.1);
+
+    switch key
+        case 'uparrow'
+            brick.MoveMotor('B',-30);
+            brick.MoveMotor
+            
+            
+        case 'downarrow'
+            brick.MoveMotor('A',60);
+
+        case 'leftarrow'
+            brick.MoveMotor('D',60);
+            
+        case 'rightarrow'
+            brick.MoveMotor('D',-60);
+
+        case 0
+            brick.StopMotor('A');
+            brick.StopMotor('D');
+
+        case 'q'
+            brick.StopMotor('A');
+            brick.StopMotor('B');
+            brick.StopMotor('C');
+            brick.StopMotor('D');
+            break;
+
+    end
+end
+CloseKeyboard();
 
 
